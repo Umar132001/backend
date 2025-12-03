6b6f256dab8ffb708a8e7eb2e0040daa730dfa5b
umar <studywork1313@gmail.com>
2025-12-03 21:57:44 +0500

    just added some comments in the reports.routes.js

--- src/routes/reports.routes.js
+++ src/routes/reports.routes.js
@@ -1,22 +1,22 @@
 import express from "express";
-import auth from "../middlewares/auth.middleware.js"; // Authentication middleware
-import * as reportsController from "../controllers/reports.controller.js"; // Reports controller
+import auth from "../middlewares/auth.middleware.js";
+import * as reportsController from "../controllers/reports.controller.js";
 
 const router = express.Router();
 
-// Get Burndown Data
+// Get Burndown Data Tasks with status: "done" + completedAt
 router.get("/burndown", auth(), reportsController.getBurndownData);
 
-// Get Team Velocity Data
+// Get Team Velocity Data asks with status: "done" + a sprint field
 router.get("/velocity", auth(), reportsController.getTeamVelocity);
 
-// Get Workload Analysis Data
+// Get Workload Analysis Data Any tasks (assigned to someone)
 router.get("/workload", auth(), reportsController.getWorkloadData);
 
-// Get Recurring Tasks Data
+// Get Recurring Tasks Data Tasks where recurring !== "none"
 router.get("/recurring", auth(), reportsController.getRecurringTasksData);
 
-// Get Time Tracking Data
+// Get Time Tracking Data Tasks with totalTrackedTime > 0
 router.get("/time-tracking", auth(), reportsController.getTimeTrackingData);
 
 export default router;
